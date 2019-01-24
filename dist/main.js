75b8815e67773417e7c1920b1a4741426412a7fb
theMugician <gslonina7@gmail.com>
2019-01-23 23:54:26 -0800

    Number animation

--- dist/main.js
+++ dist/main.js
@@ -12,33 +12,55 @@ const DOM = () => {
   return dom
 }
 
-const State = () => {
-  const state = {}
-
-  state.score = 0
-  state.isLoggedIn.facebook = 0
-  state.isLoggedIn.linkedin = 0
-
-  return state
+const State = {
+  score: 0
 }
 
 const Handler = () => {
   const handler = {}
 
-  handler.addToScore = function() { DOM.score.innerHTML = State.score }
+  handler.updateScore = function(curr, added) { 
+    let end = curr + added
+    let ticks = 20
+    let speed = 40
+
+    let randomNumbers = [end]
+
+    for (let i = 0; i < ticks - 1; i++) {
+      randomNumbers.unshift(
+        Math.floor(Math.random() * (end - curr + 1) + curr)
+      )
+    }
+
+    randomNumbers.sort((a, b) => {return a - b})
+
+    let x = 0
+    let interval = setInterval(function () {
+      
+       DOM().score.innerHTML = randomNumbers.shift()
+
+       if (++x === ticks) {
+          window.clearInterval(interval)
+       }
+
+    }, speed)
+
+    State.score = end
+  }
+
   return handler
 }
 
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-  });
+  })
 }
 
 function statusChangeCallback(response) {
   if(response.status === 'connected') {
     console.log( 'Logged in and authenticated' )
-    Handler.addToScore()
+    Handler().updateScore(State.score, 10)
   } else {
     console.log('Not authenticated')
   }
